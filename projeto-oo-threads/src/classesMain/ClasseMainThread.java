6d76c559029526ad82893c1ba9efe2f0161db2a0
rodrigo <prof-rodrigo-silva@users.noreply.github.com>
2021-11-24 14:38:08 -0200

    Add Runnable

--- projeto-oo-threads/src/classesMain/ClasseMainThread.java
+++ projeto-oo-threads/src/classesMain/ClasseMainThread.java
@@ -12,7 +12,7 @@ public class ClasseMainThread {
 			Thread.sleep(2000);
 			
 		}
-		System.out.println("FINALIZOU");*/
+		System.out.println("FINALIZOU");
 		
 		new Thread() {
 			public void run() {
@@ -38,11 +38,45 @@ public class ClasseMainThread {
 					}
 				}
 			};
-		}.start();
+		}.start();*/
+		
+		Thread email = new Thread(thread1);
+		email.start();
+		
+		Thread notaFiscal = new Thread(thread2);
+		notaFiscal.start();
 		
 		System.out.println("FINALIZOU");
 		JOptionPane.showMessageDialog(null, "Segue o fluxo");
 
 	}
+	
+	private static Runnable thread1 = new Runnable() {
+		@Override
+		public void run() {
+			for(int i = 0; i < 10; i++) {
+				System.out.println("Envio de e-mail...");
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	};
+	
+	private static Runnable thread2 = new Runnable() {
+		@Override
+		public void run() {
+			for(int i = 0; i < 10; i++) {
+				System.out.println("Envio de nota fiscal...");
+				try {
+					Thread.sleep(2000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}	
+		}
+	};
 
 }
